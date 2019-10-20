6ec763bad7be14f6280cb29abf1854678498711a
flaviofss07 <flaviofss.07@gmail.com>
2019-10-20 09:43:32 -0300

    Implementado o repository

--- src/main/java/com/gapnt/formastic/repository/EfetivoRepository.java
+++ src/main/java/com/gapnt/formastic/repository/EfetivoRepository.java
@@ -1,4 +1,19 @@
 package com.gapnt.formastic.repository;
 
-public interface EfetivoRepository {
+import com.gapnt.formastic.model.Efetivo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EfetivoRepository extends JpaRepository<Efetivo, Long> {
+
+    List<Efetivo> findByFone(String fone);
+
+    @Query("FROM Efetivo e WHERE e.fone LIKE %:fone%")
+    List<Efetivo> findByFonePart(@Param("fone") String fone);
+
 }
