bf8f42371d6ae1d504b609fce353d3724cdecf76
flaviofss07 <flaviofss.07@gmail.com>
2019-10-20 09:56:48 -0300

    Implementado o serviço

--- src/main/java/com/gapnt/formastic/service/EfetivoService.java
+++ src/main/java/com/gapnt/formastic/service/EfetivoService.java
@@ -1,5 +1,31 @@
 package com.gapnt.formastic.service;
 
+import com.gapnt.formastic.model.Efetivo;
+import com.gapnt.formastic.repository.EfetivoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EfetivoService {
 
+    @Autowired
+    private EfetivoRepository repository;
+
+    public List<Efetivo> listaTodos() {
+        return repository.findAll();
+    }
+
+    public Efetivo salva(Efetivo efetivo){
+        return repository.save(efetivo);
+    }
+
+    public void removePorId(Long id){
+        repository.deleteById(id);
+    }
+
+    public Efetivo listaPorId(Long id){
+        return repository.findById(id).get();
+    }
 }
