74749dbaf5533aeeb4184d51d751baeb78574e33
flaviofss07 <flaviofss.07@gmail.com>
2019-10-20 14:44:46 -0300

    Feito o resourse

---
+++ src/main/java/com/gapnt/formastic/resource/EfetivoResource.java
@@ -0,0 +1,45 @@
+package com.gapnt.formastic.resource;
+
+import com.gapnt.formastic.model.Efetivo;
+import com.gapnt.formastic.service.EfetivoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/efetivos-astic")
+public class EfetivoResource {
+
+    @Autowired
+    private EfetivoService service;
+
+    @GetMapping
+    public List<Efetivo> findAll(){
+        return service.listaTodos();
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<Efetivo> findById(@PathVariable("id") Long id){
+        try {
+            Efetivo efetivo = service.listaPorId(id);
+            return ResponseEntity.ok(efetivo);
+        } catch (Exception e){
+            return  ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Efetivo> cria(@RequestBody Efetivo efetivo, HttpServletResponse response){
+        Efetivo e = service.salva(efetivo);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(e.getId()).toUri();
+        response.setHeader("Location", toASCIIString());
+
+        return ResponseEntity.created(uri).body(e);
+    }
+
+}
